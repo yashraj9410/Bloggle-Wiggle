f2d0d2a260b4cb5019f02464430fb16b923a571a
Yash Raj <yashraj941015@gmail.com>
2024-01-30 16:57:29 +0530

    config env variables

--- blogFrontend/src/App.jsx
+++ blogFrontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-
+import conf from './conf/conf'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0) 
+  
   return (
     <>
       <h1>Blog appwrite</h1>
