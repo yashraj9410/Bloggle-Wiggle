9a03b040deae57574ff48acb74bc543b22d3ecab
Yash Raj <yashraj941015@gmail.com>
2024-02-04 21:47:44 +0530

    auth Slice with login logout

--- blogFrontend/src/store/authSlice.js
+++ blogFrontend/src/store/authSlice.js
@@ -6,5 +6,19 @@ const initialState ={
 }
 
 const authSlice = createSlice({
-    name:"auth"
+    name:"auth",
+    initialState,
+    reducers:{
+        login: (state,action) => {
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
+        }
+    }
 })
+
+export const {login,logout}  = authSlice.actions;
+export default authSlice.reducer;
